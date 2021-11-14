da5798e43bf7d47490f85fcc7e0501fdd9dd75f6
DimaTakarevskiy <takarevskiy.dima@mail.ru>
2021-11-14 14:47:00 +0300

    Created Main

--- Development_of_the_simplest_application_with_a_windowed_interface/src/MainPackage/Main.java
+++ Development_of_the_simplest_application_with_a_windowed_interface/src/MainPackage/Main.java
@@ -1,10 +1,15 @@
 package MainPackage;
 
 import javax.swing.*;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
+            MainFrame frame = new MainFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
 
     }
 }
